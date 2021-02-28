ab6574bf99c421bbbd00d17ecee97ebf4e9b3835
SeltanRush <akuznetsov.sel@gmail.com>
2021-02-28 17:25:49 +0700

    Updated observer logic. Added additional checks

--- main.js
+++ main.js
@@ -1,95 +1,101 @@
-const connectionsWrapperNode = document.createElementNS(
-  "http://www.w3.org/2000/svg",
-  "svg"
-);
-const connectionsWrapperNodeNS = connectionsWrapperNode.namespaceURI;
+const OBSERVER_INTERVAL = 50;
 
-document.body.appendChild(connectionsWrapperNode);
-connectionsWrapperNode.style.width = "100%";
-connectionsWrapperNode.style.height = "100%";
-connectionsWrapperNode.style.position = "absolute";
-connectionsWrapperNode.style.pointerEvents = "none";
-connectionsWrapperNode.style.top = 0;
-connectionsWrapperNode.style.left = 0;
-
-const createConnectionNode = () => {
-  const connectionNode = document.createElementNS(
-    connectionsWrapperNodeNS,
-    "line"
+const connectNodes = (() => {
+  const connectionsWrapperNode = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
   );
-  connectionsWrapperNode.appendChild(connectionNode);
-  connectionNode.setAttribute("stroke", "black");
+  connectionsWrapperNode.style.width = "100%";
+  connectionsWrapperNode.style.height = "100%";
+  connectionsWrapperNode.style.position = "fixed";
+  connectionsWrapperNode.style.pointerEvents = "none";
+  connectionsWrapperNode.style.top = 0;
+  connectionsWrapperNode.style.left = 0;
 
-  return connectionNode;
-};
+  document.body.appendChild(connectionsWrapperNode);
 
-const computeNewConnectionStyles = (firstNode, secondNode) => {
-  const firstNodeCoords = firstNode.getBoundingClientRect();
-  const secondNodeCoords = secondNode.getBoundingClientRect();
+  const createConnectionNode = (NS) => {
+    const connectionNode = document.createElementNS(NS, "line");
+    connectionsWrapperNode.appendChild(connectionNode);
+    connectionNode.setAttribute("stroke", "black");
 
-  return {
-    y1: firstNodeCoords.top,
-    y2: secondNodeCoords.top,
-    x1: firstNodeCoords.left,
-    x2: secondNodeCoords.left,
+    return connectionNode;
   };
-};
 
-const connectNodes = (firstNodeSelector, secondNodeSelector) => {
-  const firstNode = document.querySelector(firstNodeSelector);
-  const secondNode = document.querySelector(secondNodeSelector);
+  const computeNewConnectionStyles = (firstNode, secondNode) => {
+    const firstNodeCoords = firstNode.getBoundingClientRect();
+    const secondNodeCoords = secondNode.getBoundingClientRect();
 
-  const connectionNode = createConnectionNode();
+    return {
+      y1: firstNodeCoords.top,
+      y2: secondNodeCoords.top,
+      x1: firstNodeCoords.left,
+      x2: secondNodeCoords.left,
+    };
+  };
 
-  if (!firstNode || !secondNode) {
-    throw new Error("Cannot find elements!");
-  }
+  return (firstNodeSelector, secondNodeSelector) => {
+    const firstNode = document.querySelector(firstNodeSelector);
+    const secondNode = document.querySelector(secondNodeSelector);
 
-  const renderConnection = () => {
-    const { x1, x2, y1, y2 } = computeNewConnectionStyles(
-      firstNode,
-      secondNode
+    const connectionNode = createConnectionNode(
+      connectionsWrapperNode.namespaceURI
     );
-    console.log(x1, x2, y1, y2);
-    const step = Math.sqrt(x1 * x2 + y1 * y2) / 100;
 
-    connectionNode.setAttribute("x1", x1);
-    connectionNode.setAttribute("x2", x2);
-    connectionNode.setAttribute("y1", y1);
-    connectionNode.setAttribute("y2", y2);
-  };
+    if (!firstNode || !secondNode) {
+      throw new Error("Cannot find elements!");
+    }
 
-  const removeConnection = () => {
-    connectionNode.remove();
-  };
+    if (
+      connectionsWrapperNode.isSameNode(firstNode) ||
+      connectionsWrapperNode.isSameNode(secondNode)
+    ) {
+      throw new Error("Cannot build connection for these elements");
+    }
 
-  const isNodesExistsInBody = () => {
-    return (
-      document.body.contains(firstNode) && document.body.contains(secondNode)
-    );
-  };
+    if (
+      connectionsWrapperNode.contains(firstNode) ||
+      connectionsWrapperNode.contains(secondNode)
+    ) {
+      throw new Error("Cannot build connection for these elements");
+    }
 
-  const observer = new MutationObserver((mutations, observer) => {
-    const isMutationOnConnections = mutations.some((m) =>
-      connectionsWrapperNode.contains(m.target)
-    );
-    if (!isMutationOnConnections) {
-      if (firstNode && secondNode && isNodesExistsInBody()) {
+    const renderConnection = () => {
+      const { x1, x2, y1, y2 } = computeNewConnectionStyles(
+        firstNode,
+        secondNode
+      );
+
+      const isPositionChanged =
+        connectionNode.attributes.x1 !== x1 ||
+        connectionNode.attributes.x2 !== x2 ||
+        connectionNode.attributes.y1 !== y1 ||
+        connectionNode.attributes.y2 !== y2;
+
+      if (isPositionChanged) {
+        connectionNode.setAttribute("x1", x1);
+        connectionNode.setAttribute("x2", x2);
+        connectionNode.setAttribute("y1", y1);
+        connectionNode.setAttribute("y2", y2);
+      }
+    };
+
+    const removeConnection = () => {
+      connectionNode.remove();
+    };
+
+    const observer = setInterval(() => {
+      if (
+        document.body.contains(firstNode) &&
+        document.body.contains(secondNode)
+      ) {
         renderConnection();
       } else {
-        observer.disconnect();
         removeConnection();
+        clearInterval(observer);
       }
-    }
-  });
+    }, [OBSERVER_INTERVAL]);
 
-  observer.observe(document.body, {
-    attributes: true,
-    characterData: true,
-    childList: true,
-    subtree: true,
-  });
-
-  firstNode.addEventListener("transitionrun", renderConnection);
-  renderConnection();
-};
+    renderConnection();
+  };
+})();
